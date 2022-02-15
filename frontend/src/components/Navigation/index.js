4562f981352fdd648de4ed8840a08306d17efb88
carlos rodriguez <twincarlos98@gmail.com>
2022-02-15 17:06:23 -0500

    nav bar styled almost done

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import logo from '../../assets/logo.png';
+import * as sessionActions from '../../store/session';
+
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
+  const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
+
+  const handleDemo = () => {
+    return dispatch(sessionActions.login({ credential: 'demo@user.io', password: 'password' }));
+  }
 
   let sessionLinks;
   if (sessionUser) {
@@ -15,16 +23,18 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <input type='text' placeholder='Search'></input>
+        <NavLink id='demo' onClick={() => handleDemo()} to="/">Demo</NavLink>
+        <NavLink id='a-login' to="/login">Log In</NavLink>
+        <NavLink id='a-signup' to="/signup">Sign Up</NavLink>
       </>
     );
   }
 
   return (
-    <ul>
+    <ul id='navbar'>
       <li>
-        <NavLink exact to="/">Home</NavLink>
+        <NavLink id='logo-home' exact to="/"><img src={logo} alt=''></img></NavLink>
         {isLoaded && sessionLinks}
       </li>
     </ul>
