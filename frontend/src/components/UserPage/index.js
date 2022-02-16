b208c43dee3bf9ebfb583084106694fba5b9fbd6
carlos rodriguez <twincarlos98@gmail.com>
2022-02-16 03:06:38 -0500

    so far stable no bugs code

--- frontend/src/components/UserPage/index.js
+++ frontend/src/components/UserPage/index.js
@@ -1,14 +1,30 @@
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './UserPage.css';
 
 function UserPage () {
+    const userId = useParams().id;
+    const sessionUser = useSelector(state => state.session.user);
+
     return (
         <div id='user-header'>
             <img src='https://routenote.com/blog/wp-content/uploads/2022/01/243283253_580988179688935_8877892167513690479_n.jpg' alt=''></img>
             <div>
                 <div id='user-header-top'>
                     <h1>arianagrande</h1>
-                    <button type='submit'>Edit Profile</button>
-                    <i className="fas fa-cog"></i>
+                    {
+                        (sessionUser && sessionUser?.id === Number(userId))
+                        ?
+                        <span>
+                            <button type='submit'>Edit Profile</button>
+                            <i className="fas fa-cog"></i>
+                        </span>
+                        :
+                        <span className='not-my-profile'>
+                            <button type='submit'>Follow</button>
+                            <i className="fas fa-ellipsis-h"></i>
+                        </span>
+                    }
                 </div>
                 <div id='user-header-middle'>
                     <p><b>4,936</b> posts</p>
@@ -18,7 +34,7 @@ function UserPage () {
                 <div id='user-header-bottom'>
                     <p><b>Ariana Grande</b></p>
                     <a target="_blank" rel="noreferrer" href='https://rembeauty.com/'>rembauty.com</a>
-                    <p id='followed-by'>Followed by <b>username</b> <b>username</b> <b>username</b> +158 more</p>
+                    <p id='followed-by'>Followed by <b>username</b>, <b>username</b>, <b>username</b> +158 more</p>
                 </div>
             </div>
         </div>
